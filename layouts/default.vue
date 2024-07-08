b090b3532371cb129cc0882c42db1174b287c52b
Samuel Ebuka <samuel.ebuka@gmail.com>
2024-07-08 12:42:16 +0200

    chore(admin): admin config init

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,8 +13,20 @@ useHead({
 // Optional: Log when the component is mounted
 onMounted(() => {
   console.log('Component mounted, Netlify Identity script should be loaded')
-})
   
+  // Initialize Netlify Identity
+  if (window.netlifyIdentity) {
+    window.netlifyIdentity.on("init", user => {
+      if (!user) {
+        window.netlifyIdentity.on("login", () => {
+          document.location.href = "/admin/";
+        });
+      }
+    });
+  } else {
+    console.warn('Netlify Identity widget not loaded');
+  }
+})
 </script>
 <template>
   <div>
