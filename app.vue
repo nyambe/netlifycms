8d15b14654c862d47d8dd9399e22d40692c6482d
Samuel Ebuka <samuel.ebuka@gmail.com>
2024-07-08 11:23:20 +0200

    feature(admin): admin for netfilfy cms

--- app.vue
+++ app.vue
@@ -1,3 +1,13 @@
+<script setup>
+onMounted(() => {
+  if (process.client) {
+    const script = document.createElement('script')
+    script.src = 'https://identity.netlify.com/v1/netlify-identity-widget.js'
+    document.head.appendChild(script)
+  }
+})
+</script>
+
 <template>
   <div>
     <LandingPage />
